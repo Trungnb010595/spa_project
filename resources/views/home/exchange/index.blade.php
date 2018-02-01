af7435aa4d0a4057ae0965e5a42754c61649f42b
trungnb <trungnb@kaopiz.com>
2018-02-01 08:30:00 +0700

    update

--- resources/views/home/exchange/index.blade.php
+++ resources/views/home/exchange/index.blade.php
@@ -7,7 +7,7 @@
 @endsection
 @section('content')
     <h1>Wellcome to Spa Club - Exchange!!!</h1>
-    <div class="col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3">
+    <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-primary ">
             <div class="panel-heading">Giao dịch <a  class="pull-right btn btn-primary btn-xs" href="{{ route('exchange.add') }}">Thêm giao dịch</a></div>
             <div class="panel-body">
@@ -17,7 +17,9 @@
                     <th class="text-center">Tên khách hàng</th>
                     <th class="text-center">Tên nhân viên</th>
                     <th class="text-center">Tên sản phẩm</th>
-                    <th class="text-center"></th>
+                    <th class="text-center">Tên dịch vụ</th>
+                    <th class="text-center">Sửa</th>
+                    <th class="text-center">Xóa</th>
                     </thead>
                     <tbody>
 
@@ -28,10 +30,13 @@
                             <td class="text-center">{{ \App\Customer::find($exchange->cus_id)->name }}</td>
                             <td class="text-center">{{ \App\Employee::find($exchange->emp_id)->name  }}</td>
                             <td class="text-center">{{ \App\Product::find($exchange->product_id)->name  }}</td>
+                            <td class="text-center">{{ \App\Service::find($exchange->service_id)->name  }}</td>
                             <td class="text-center">
                                 <a href="{{route('exchange.edit',['id' => $exchange->id])}}" class="btn btn-xs btn-success">
                                     <i class="fa fa-pencil" aria-hidden="true"></i>
                                 </a>
+                            </td>
+                            <td class="text-center">
                                 <a href="{{route('exchange.delete',['id' => $exchange->id])}}" class="btn btn-xs btn-success">
                                     <i class="fa fa-trash-o" aria-hidden="true"></i>
                                 </a>
