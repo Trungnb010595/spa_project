2a9ac4a758d1de2abae24e9d1ce30c870736bec4
trungnb <trungnb@kaopiz.com>
2018-02-05 13:13:43 +0700

    update calculate

--- app/Libs/common.php
+++ app/Libs/common.php
@@ -1,52 +1,12 @@
 <?php
-//tien nhan duoc chua tru di tien cong nhan vien
-function getMoneyReceived(){
-    $exchanges = \App\Exchange::all();
-    $money = 0;
-    foreach ($exchanges as $exchange){
-        $check_date = false;
-        if(date("m", strtotime($exchange->created_at)) == date('m')
-            && date("Y", strtotime($exchange->created_at)) == date('Y')){
-            $check_date = true;
-        }
-        if($check_date){
-            if($exchange->product_id){
-                $product = \App\Product::find($exchange->product_id);
-                $money = $money + $product->price_export - $product->price_import;
-            }
-            if($exchange->service_id){
-                $service = \App\Service::find($exchange->service_id);
-                $money = $money + $service->price;
-            }
-        }
-    }
-    return $money;
-}
+
 //Tien nhan duoc da tru di tien cong cua nhan vien
-function getMoneyReceived2(){
-    $exchanges = \App\Exchange::all();
-    $money = 0;
-    foreach ($exchanges as $exchange){
-        $check_date = false;
-        if(date("m", strtotime($exchange->created_at)) == date('m')
-            && date("Y", strtotime($exchange->created_at)) == date('Y')){
-            $check_date = true;
-        }
-        if($check_date){
-            if($exchange->product_id){
-                $product = \App\Product::find($exchange->product_id);
-                $money = $money + $product->price_export - $product->price_import;
-            }
-            if($exchange->service_id){
-                $service = \App\Service::find($exchange->service_id);
-                $money = $money + $service->price - $service->price*$service->bonus_for_emp/100;
-            }
-        }
-    }
+function getMoneyReceived(){
+    $money = getMoneyReceivedByServices() + getMoneyReceivedByProducts() - sumSalary();
     return $money;
 }
 
-function countSalary(){
+function sumSalary(){
     $employees = \App\Employee::all();
     $sum = 0;
     foreach ($employees as $employee){
@@ -64,7 +24,7 @@ function getMoneyReceivedByServices(){
             $check_date = true;
         }
         if($check_date){
-            if($exchange->service_id){
+            if($exchange->service_id && $exchange->service_quantity){
                 $service = \App\Service::find($exchange->service_id);
                 $money = $money + $service->price;
             }
@@ -82,9 +42,9 @@ function getMoneyReceivedByProducts(){
             $check_date = true;
         }
         if($check_date){
-            if($exchange->product_id){
+            if($exchange->product_id && $exchange->product_quantity){
                 $product = \App\Product::find($exchange->product_id);
-                $money = $money + $product->price_export - $product->price_import;
+                $money = $money + ($product->price_export - $product->price_import)*$exchange->product_id;
             }
         }
     }
